8c520d9471059aedd7eb6aa40606b18d0ad278de
Alissa Wiley <alissa.estelle.w@gmail.com>
2022-05-15 02:17:39 -0400

    Acct Page Formatted

--- headspace_project/client/src/index.js
+++ headspace_project/client/src/index.js
@@ -13,4 +13,4 @@ root.render(
   </React.StrictMode>
 )
 
-// COMMIT COUNT: 40
+// COMMIT COUNT: 41
--- headspace_project/client/src/pages/Account.js
+++ headspace_project/client/src/pages/Account.js
@@ -1,5 +1,37 @@
 const Account = () => {
-  return <div className="Account"></div>
+  return (
+    <div className="Acct-Container">
+      <div className="Acct-Grid">
+        <form onSubmit="">
+          <h1>
+            <span id="Change">Change</span> <span id="Password">Password</span>
+          </h1>
+          <input
+            required
+            type="password"
+            placeholder="Old Password"
+            value=""
+            onChange=""
+          ></input>
+          <input
+            required
+            type="password"
+            placeholder="New Password"
+            value=""
+            onChange=""
+          ></input>
+          <button id="Edit">Submit</button>
+        </form>
+        <div>
+          <h1>
+            <span id="Saying">Saying</span>
+            <span id="Farewell">Farewell?</span>
+          </h1>
+          <button id="Delete">Delete</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Account
